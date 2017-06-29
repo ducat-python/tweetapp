f600263974d0b6726354c77d44e70867f07192a2
Piyush <ducat-python@outlook.com>
2017-06-29 12:53:39 +0530

    Delete added

--- tweets/urls.py
+++ tweets/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^create/', TweetCreateView.as_view(), name='create'),
     url(r'^detail/(?P<pk>\d+)$', TweetDetailView.as_view(), name='detail'),
     url(r'^update/(?P<pk>\d+)$', TweetUpdateView.as_view(), name='update'),
-    url(r'^update/(?P<pk>\d+)$', TweetUpdateView.as_view(), name='update'),
+    url(r'^delete/(?P<pk>\d+)$', TweetDeleteView.as_view(), name='delete'),
 
 ]
